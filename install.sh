75be6ce87fadda8ef3557a3dafb74d3e9ca046f4
rishikant42 <rksbtp@gmail.com>
2018-06-08 12:02:44 +0530

    added man page

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ source /usr/local/bin/cryptoenv/bin/activate
 pip install -r requirements.txt
 chmod a+x cryptoo
 cp cryptoo /usr/local/bin/
+cp cryptoo.1 /usr/local/share/man/man1/
 echo "cryptoo CLI is successfully installed"
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf /usr/local/bin/cryptoenv
 rm /usr/local/bin/cryptoo
+rm /usr/local/share/man/man1/cryptoo.1
 echo "cryptoo CLI is successfully uninstalled"
