a5cbc15de27d1abc3619f7759d8e3b5319551993
rishikant42 <rksbtp@gmail.com>
2018-06-09 10:46:00 +0530

    updated installer

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-pip install virtualenv
-virtualenv /usr/local/bin/cryptoenv
-source /usr/local/bin/cryptoenv/bin/activate
-pip install -r requirements.txt
 chmod a+x cryptoo
 cp cryptoo /usr/local/bin/
 cp cryptoo.1 /usr/local/share/man/man1/
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -rf /usr/local/bin/cryptoenv
 rm /usr/local/bin/cryptoo
 rm /usr/local/share/man/man1/cryptoo.1
 echo "cryptoo CLI is successfully uninstalled"
